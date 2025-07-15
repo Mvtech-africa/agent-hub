945154607bf463facef5bb9f0c0a701d702dcd33
mvtech <mvtech44@gmail.com>
2025-07-15 13:10:28 +0100

    Modified css and html

--- server/realtor/urls.py
+++ server/realtor/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-from .views import index
+from django.urls import path, include
+from .views import index, sign_in, sign_up, upload
 
 urlpatterns = [
     path('', index, name='realtor-index'),  # URL for the index view 
-    
+    path('sign-up', sign_up, name='sign-up'),
+    path('sign-in', sign_in, name='sign-in'),
+    path('upload', upload, name='upload')
     # You can add more paths here for other views in the realtor app
     # path('another-view/', another_view, name='another-view'), 
     # path('property/<int:id>/', property_detail, name='property-detail'),
--- server/realtor/views.py
+++ server/realtor/views.py
@@ -6,3 +6,13 @@ def index (request):
     Render the index page for the realtor app.
     """
     return render(request, 'index.html')
+
+def sign_up(request):
+
+    return render(request, 'sign-up.html')
+
+def sign_in(request):
+    return render (request, 'sign-in.html')
+
+def upload(request):
+    return render(request, 'upload.html')
